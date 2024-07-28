5c1e992124faf722da6fb946e503ae19dc03daab
Nagarajan R <141135525+Nagarajan0410@users.noreply.github.com>
2024-07-28 23:48:27 +0530

    Add files via upload

---
+++ TicTacToeApplication/src/test/java/com/example/TicTacToeApplication/TicTacToeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TicTacToeApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
